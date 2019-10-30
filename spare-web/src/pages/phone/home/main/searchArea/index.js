967b7aff6e2ab8a603d23a4c16f6298edf85ffa9
qihua.li <qihua.li@wehotelglobal.com>
2019-10-30 18:14:30 +0800

    liqihua

--- spare-web/src/pages/phone/home/main/searchArea/index.js
+++ spare-web/src/pages/phone/home/main/searchArea/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import {SearchBar} from 'antd-mobile'
+import btnCategoryList from '@/static/images/btn-category-list.png'
+import './style.css'
+import { Flex, WhiteSpace } from 'antd-mobile';
 
 class SearchArea extends React.Component {
     render() {
         return (
-            <div>
-                <SearchBar placeholer="商品/店铺"/>
+            <div className='searchArea'>
+                <Flex>
+                    <Flex.Item><div className='searchArea-left'>1</div></Flex.Item>
+                    <Flex.Item><div className='searchArea-middle'>2</div></Flex.Item>
+                    <Flex.Item><div className='searchArea-right'>3</div></Flex.Item>
+                </Flex>
             </div>
         )
     }
