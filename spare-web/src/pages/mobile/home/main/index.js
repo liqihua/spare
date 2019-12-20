f172977373b2f635a7d10f9301fb906fa6efcfaa
qihua.li <qihua.li@wehotelglobal.com>
2019-12-20 10:20:14 +0800

    liqihua

--- spare-web/src/pages/mobile/home/main/index.js
+++ spare-web/src/pages/mobile/home/main/index.js
@@ -1,39 +1,76 @@
 import React from 'react'
 import './style.css'
 import SearchArea from './searchArea'
-import { Button } from 'antd-mobile'
+import { Carousel, WingBlank } from 'antd-mobile'
+import iconSearch from '@/static/mobile/images/carousel/1.jpg'
 
 
 class Main extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: [],
+            imgHeight: null,
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
+            });
+        }, 100);
+    }
+
     render() {
         return (
             <div className='main'>
                 <div>
-                    <SearchArea showSearchPage={this.showSearchPage.bind(this)}/>
+                    <SearchArea showSearchPage={this.showSearchPage.bind(this)} />
+                        <Carousel
+                            autoplay={true}
+                            infinite
+                        >
+                            {this.state.data.map(val => (
+                                <a
+                                    key={val}
+                                    href="http://www.alipay.com"
+                                    style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+                                >
+                                    <img
+                                        src={iconSearch}
+                                        alt=""
+                                        style={{ width: '100%', verticalAlign: 'top' }}
+                                        onLoad={() => {
+                                            window.dispatchEvent(new Event('resize'));
+                                            this.setState({ imgHeight: 'auto' });
+                                        }}
+                                    />
+                                </a>
+                            ))}
+                        </Carousel>
                 </div>
-                <div>
-                    <Button>123</Button>
-                </div>
-                <div>main</div>
-                <div>main</div>
-                <div>main</div>
-                <div>main</div>
-                <div>main</div>
-                <div>main</div>
-                <div>main</div>
-                <div>main</div>
-                <div>main</div>
-                <div>main</div>
-                <div>main</div>
-                <div>main</div>
-                <div>main</div>
-                <div>main</div>
-                <div>main</div>
-                <div>main</div>
+                <div className='test'>main</div>
+                <div className='test'>main</div>
+                <div className='test'>main</div>
+                <div className='test'>main</div>
+                <div className='test'>main</div>
+                <div className='test'>main</div>
+                <div className='test'>main</div>
+                <div className='test'>main</div>
+                <div className='test'>main</div>
+                <div className='test'>main</div>
+                <div className='test'>main</div>
+                <div className='test'>main</div>
+                <div className='test'>main</div>
+                <div className='test'>main</div>
+                <div className='test'>main</div>
+                <div className='test'>main</div>
+                <div className='test'>main</div>
             </div>
         )
     }
-    
+
     showSearchPage() {
         this.props.showSearchPage()
     }
@@ -41,7 +78,7 @@ class Main extends React.Component {
 }
 
 
-    
+
 
 
 
